9f2fc4b2e8f4147e563d472dd002593ea367bdb6
bonanza9 <dom@thinkpigment.com>
2011-04-08 15:38:33 +0100

    Adds markup to textfield examples

--- js/script.js
+++ js/script.js
@@ -2,8 +2,12 @@ $(function(){
   $('#hide-css').remove();
   $('.js-hide').hide();
   
-  $('input[type=submit], button, a.button').click(function(){
-    $(this).parents(".example:first").find('div.js-hide').slideToggle('fast');
+  $('input[type=submit], input[type=reset], button, .button, input[type=text], textarea').click(function(){
+    showMarkup(this);
     return false;
   });
+  
+  function showMarkup(el) {
+    $(el).parents(".example:first").find('div.js-hide').slideToggle('fast');
+  }
 });
